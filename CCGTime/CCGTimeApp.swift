101786340c958415c2cdd3ac7aa6eac6656d69d2
Ben Rosario <b.enn@icloud.com>
2023-01-13 10:29:21 -0800

    Initial Commit

---
+++ CCGTime/CCGTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCGTimeApp.swift
+//  CCGTime
+//
+//  Created by Ben on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CCGTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
